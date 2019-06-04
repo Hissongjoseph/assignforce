a0b0860ed10d24cee18bfff4e25d4f2629ddfb8d
Hissongjoseph <Hissong.joseph@gmail.com>
2019-06-04 13:32:41 -0400

    Integrated Curriculum and Batch into Resources

--- assignforce-batch-service/src/main/java/com/revature/assignforce/BatchServiceApplication.java
+++ assignforce-batch-service/src/main/java/com/revature/assignforce/BatchServiceApplication.java
@@ -3,8 +3,7 @@ package com.revature.assignforce;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+çimport org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 
--- assignforce-resource-service/src/main/java/com/revature/assignforce/AssignforceResourceServiceApplication.java
+++ assignforce-resource-service/src/main/java/com/revature/assignforce/AssignforceResourceServiceApplication.java
@@ -2,8 +2,16 @@ package com.revature.assignforce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableHystrix
+@EnableWebSecurity
 public class AssignforceResourceServiceApplication {
 
     public static void main(String[] args) {
