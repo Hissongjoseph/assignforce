c5f71d81f182618bd34f8860531824c0090e728c
Jose Nunez <901jnunez@gmail.com>
2019-06-04 11:39:17 -0400

    added resource service

---
+++ assignforce-resource-service/src/main/java/com/revature/assignforce/AssignforceResourceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.revature.assignforce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignforceResourceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignforceResourceServiceApplication.class, args);
+    }
+
+}
