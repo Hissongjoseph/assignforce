8a8e94d432cd090417797bf0e50df0160311139e
Hissongjoseph <Hissong.joseph@gmail.com>
2019-06-07 11:58:31 -0400

    Commented out SkillMessage for now

--- assignforce-resource-service/src/main/java/com/revature/assignforce/SkillMessage.java
+++ assignforce-resource-service/src/main/java/com/revature/assignforce/SkillMessage.java
@@ -1,17 +1,17 @@
-package com.revature.assignforce;
-
-public class SkillMessage {
-    private int skillId;
-
-    public SkillMessage() {
-        super();
-    }
-
-    public int getSkillId() {
-        return skillId;
-    }
-
-    public void setSkillId(int skillId) {
-        this.skillId = skillId;
-    }
-}
+//package com.revature.assignforce;
+//
+//public class SkillMessage {
+//    private int skillId;
+//
+//    public SkillMessage() {
+//        super();
+//    }
+//
+//    public int getSkillId() {
+//        return skillId;
+//    }
+//
+//    public void setSkillId(int skillId) {
+//        this.skillId = skillId;
+//    }
+//}
